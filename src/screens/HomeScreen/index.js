b528d15a5eaf4b36302103596367870e2f038d74
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-04 00:49:54 +0200

    mock screen Home

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Stories from '../../components/Stories';
 import Feed from '../../components/Feed';
+import { SafeAreaView } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <>
+    <SafeAreaView>
       <Feed />
-    </>
+    </SafeAreaView>
   );
 };
 
