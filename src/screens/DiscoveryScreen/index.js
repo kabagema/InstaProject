39a83287e04928d6b1be028dd9a90713974c6be5
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-04 00:49:39 +0200

    mock screen discovery

---
+++ src/screens/DiscoveryScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const DiscoveryScreen = () => (
+    
+       <View style={{backgroundColor: "#fcba03", height: 1500}}>
+           <Text style={{textAlign: 'center', marginTop: 300, fontSize: 30, color: 'white'}}>Discovery</Text>
+       </View>
+)
+
+export default DiscoveryScreen;
