e477477989b62c5ff1f1fda1b67ab7ce8bc6999a
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-03 12:46:12 +0200

    likes caption and time

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
-
-const Footer = () => 
+import { View, Text } from 'react-native';
+import styles from './styles';
+const Footer = ({likesCount, caption, postedAt}) => 
 (
-    <Text>
-        Footer
-    </Text>
+    <View style={styles.container}>
+        <Text style={styles.likes}>
+            {likesCount} Likes
+        </Text>
+        <Text  style={styles.caption}>
+            {caption}
+        </Text>
+        <Text style={styles.postedAt}>
+            {postedAt}
+        </Text>
+    </View>
 )
 
 export default Footer;
