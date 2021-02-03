dffbd152a496b8d6a451b34f60db88b18b75dcf8
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-04 00:49:12 +0200

    mock screen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,18 +8,34 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
 import ProfilePicture from './components/ProfilePicture';
 import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer} from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import DiscoveryScreen from './screens/DiscoveryScreen';
+import NotificationScreen from './screens/NotificationScreen';
+import PostScreen from './screens/PostScreen';
+import ProfileScreen from './screens/Profile';
+
+
+
+const Tab = createBottomTabNavigator();
+
 
 const App: () => React$Node = () => {
+
   return (
-    <>
-      <SafeAreaView>
-        <HomeScreen />
-        {/* <ProfilePicture /> */}
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" />
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Discover" component={DiscoveryScreen} />
+        <Tab.Screen name="Post" component={PostScreen} />
+        <Tab.Screen name="Notification" component={NotificationScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+      </NavigationContainer>
   );
 };
 
