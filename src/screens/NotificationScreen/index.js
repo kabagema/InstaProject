21b9f85f30e1a08ea8f15834f49680e8127daddd
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-04 00:50:11 +0200

    mock screen notification

---
+++ src/screens/NotificationScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const NotificationScreen = () => (
+    
+       <View style={{backgroundColor: "#2fa814", height: 1500}}>
+           <Text style={{textAlign: 'center', marginTop: 300, fontSize: 30, color: 'white'}}>Notification</Text>
+       </View>
+)
+
+export default NotificationScreen;
