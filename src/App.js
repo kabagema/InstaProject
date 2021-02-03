48945364f50e5be2aa4a01f9c2bfccf89f454dec
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-04 01:27:26 +0200

    adding icons on nav

--- src/App.js
+++ src/App.js
@@ -17,18 +17,44 @@ import DiscoveryScreen from './screens/DiscoveryScreen';
 import NotificationScreen from './screens/NotificationScreen';
 import PostScreen from './screens/PostScreen';
 import ProfileScreen from './screens/Profile';
-
-
+import SearchBar from 'react-native-vector-icons/AntDesign';
+import HomeBar from 'react-native-vector-icons/Foundation';
+import PostBar from 'react-native-vector-icons/Feather';
+import ProfileBar from 'react-native-vector-icons/Ionicons';
+import HeartBar from 'react-native-vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
-
 
 const App: () => React$Node = () => {
 
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
-      <Tab.Navigator>
+      <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                if (route.name === 'Home') {
+                  return  <HomeBar name="home" size={size} color={color} />;
+                } 
+                if (route.name === 'Discover') {
+                  return  <SearchBar name="search1" size={size} color={color} />;
+                }
+                if (route.name === 'Post') {
+                  return  <PostBar name="plus-square" size={size} color={color} />;
+                }
+                if (route.name === 'Notification') {
+                  return  <HeartBar name="hearto" size={size} color={color} />;
+                }
+                if (route.name === 'Profile') {
+                  return  <ProfileBar name="person-circle-outline" size={size} color={color} />;
+                }
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: '#000',
+              inactiveTintColor: 'black',
+            }}   
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Discover" component={DiscoveryScreen} />
         <Tab.Screen name="Post" component={PostScreen} />
