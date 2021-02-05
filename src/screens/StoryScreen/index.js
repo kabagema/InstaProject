62d2c846c0e767ca9a1decba514607d317555d70
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-05 02:10:45 +0200

    day 3

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -1,6 +1,6 @@
 import { useRoute } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import {ActivityIndicator, ImageBackground, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
+import {ActivityIndicator, Dimensions, ImageBackground, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
 import styles from './styles';
 
 import storiesData from '../../data/storiesData'
@@ -21,12 +21,45 @@ const StoryScreen = () =>
     }, []);
     
     useEffect(() => {
-        if(userStories && userStories.stories.length > activeStoryIndex - 1) {
-            setactiveStory(userStories.stories[activeStoryIndex]);
+        if(!userStories) {
+            return;
         }
+        if(activeStoryIndex < 0) {
+            setactiveStoryIndex(0)
+            return;
+        }
+        if(activeStoryIndex >= userStories.stories.length) {
+            setactiveStoryIndex( userStories.stories.length - 1)
+            return;
+        }
+
+        // if(userStories && userStories.stories.length > activeStoryIndex - 1) {
+            setactiveStory(userStories.stories[activeStoryIndex]);
+        // }
         
     }, [activeStoryIndex])
     
+    const handleNextStory = () => {
+        setactiveStoryIndex( activeStoryIndex + 1 );
+    }
+
+    const handlePrevStories = () => {
+        setactiveStoryIndex( activeStoryIndex - 1 );
+    }
+
+    const handlePress = (evt) => {
+        const x = evt.nativeEvent.locationX;
+        const screenWidth = Dimensions.get('window').width;
+        let isRight = true;
+        if(x < screenWidth / 2) {
+            handlePrevStories();
+        } else {
+            handleNextStory();
+            console.log("isRight"+ isRight)
+        }
+        
+    }
+
     if(!activeStory) {
         return (
             <SafeAreaView>
@@ -36,10 +69,15 @@ const StoryScreen = () =>
     }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <ImageBackground source={{uri: activeStory.imageUri}} style={styles.image} />
+        <SafeAreaView style={styles.container}>    
+                <TouchableWithoutFeedback onPress={handlePress}>
+                    <ImageBackground source={{uri: activeStory.imageUri}} style={styles.image}>
 
+                    </ImageBackground>
+                </TouchableWithoutFeedback>
         </SafeAreaView>
     );
 }
 export default StoryScreen;
+
+//https://youtu.be/NkwMT3O4NE0?t=4665
