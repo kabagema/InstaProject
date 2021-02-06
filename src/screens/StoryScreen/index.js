7fee70cd3d9cd45e96b0f7ed955dc64bf64788f2
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-06 15:19:42 +0200

    new profile pic style

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -1,83 +1,97 @@
-import { useRoute } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import {ActivityIndicator, Dimensions, ImageBackground, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+
+import {
+  ActivityIndicator,
+  Dimensions,
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import styles from './styles';
+import storiesData from '../../data/storiesData';
+import ProfilePicture from '../../components/ProfilePicture';
 
-import storiesData from '../../data/storiesData'
-const StoryScreen = () => 
-{
-    const [userStories, setUserStories] = useState(null);
-    const [activeStoryIndex, setactiveStoryIndex] = useState(null);
-    const [activeStory, setactiveStory] = useState(null);
+const StoryScreen = () => {
+  const [userStories, setUserStories] = useState(null);
+  const [activeStoryIndex, setactiveStoryIndex] = useState(null);
+  // const [activeStory, setactiveStory] = useState(null);
 
-    const route = useRoute();
+  const route = useRoute();
+  const navigation = useNavigation();
+  const userId = route.params.userId;
 
-    useEffect(() => {
-        const userId = route.params.userId;
-        const userStories = storiesData.find(storyData => storyData.user.id === userId);
-        setUserStories(userStories)
-        setactiveStoryIndex(0)
-        
-    }, []);
-    
-    useEffect(() => {
-        if(!userStories) {
-            return;
-        }
-        if(activeStoryIndex < 0) {
-            setactiveStoryIndex(0)
-            return;
-        }
-        if(activeStoryIndex >= userStories.stories.length) {
-            setactiveStoryIndex( userStories.stories.length - 1)
-            return;
-        }
-
-        // if(userStories && userStories.stories.length > activeStoryIndex - 1) {
-            setactiveStory(userStories.stories[activeStoryIndex]);
-        // }
-        
-    }, [activeStoryIndex])
-    
-    const handleNextStory = () => {
-        setactiveStoryIndex( activeStoryIndex + 1 );
-    }
-
-    const handlePrevStories = () => {
-        setactiveStoryIndex( activeStoryIndex - 1 );
-    }
-
-    const handlePress = (evt) => {
-        const x = evt.nativeEvent.locationX;
-        const screenWidth = Dimensions.get('window').width;
-        let isRight = true;
-        if(x < screenWidth / 2) {
-            handlePrevStories();
-        } else {
-            handleNextStory();
-            console.log("isRight"+ isRight)
-        }
-        
-    }
-
-    if(!activeStory) {
-        return (
-            <SafeAreaView>
-               <ActivityIndicator />
-            </SafeAreaView>
-        );
-    }
-
-    return (
-        <SafeAreaView style={styles.container}>    
-                <TouchableWithoutFeedback onPress={handlePress}>
-                    <ImageBackground source={{uri: activeStory.imageUri}} style={styles.image}>
-
-                    </ImageBackground>
-                </TouchableWithoutFeedback>
-        </SafeAreaView>
+  useEffect(() => {
+    const userStories = storiesData.find(
+      (storyData) => storyData.user.id === userId,
     );
-}
+    setUserStories(userStories);
+    setactiveStoryIndex(0);
+  }, []);
+
+  const navigateToNextUser = () => {
+    navigation.push('Story', {userId: (parseInt(userId) + 1).toString()});
+  };
+
+  const navigateToPrevUser = () => {
+    navigation.push('Story', {userId: (parseInt(userId) - 1).toString()});
+  };
+
+  const handleNextStory = () => {
+    if (activeStoryIndex >= userStories.stories.length - 1) {
+      navigateToNextUser();
+      return;
+    }
+    setactiveStoryIndex(activeStoryIndex + 1);
+  };
+
+  const handlePrevStory = () => {
+    if (activeStoryIndex <= 0) {
+      navigateToPrevUser();
+      return;
+    }
+    setactiveStoryIndex(activeStoryIndex - 1);
+  };
+
+  const handlePress = (evt) => {
+    const x = evt.nativeEvent.locationX;
+    const screenWidth = Dimensions.get('window').width;
+
+    if (x < screenWidth / 2) {
+      handlePrevStory();
+    } else {
+      handleNextStory();
+    }
+  };
+
+  if (!userStories) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
+  const activeStory = userStories.stories[activeStoryIndex];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={handlePress}>
+        <ImageBackground
+          source={{uri: activeStory.imageUri}}
+          style={styles.image}>
+          <View style={styles.userInfo}>
+            <ProfilePicture uri={userStories.user.imageUri} size={45} />
+            <Text style={styles.userName}>{userStories.user.name}</Text>
+          </View>
+          <View></View>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  );
+};
 export default StoryScreen;
 
 //https://youtu.be/NkwMT3O4NE0?t=4665
