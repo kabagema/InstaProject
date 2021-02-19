626d0f4d0c7e538512bcd115dc04b5fd719ffa31
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-19 23:46:57 +0200

    imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {StatusBar} from 'react-native';
 
-import { NavigationContainer} from "@react-navigation/native";
+import {NavigationContainer} from '@react-navigation/native';
 import Router from './Router';
+
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 // import Router from "./Router"
 const App: () => React$Node = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
-        <Router />
-      </NavigationContainer>
+      <Router />
+    </NavigationContainer>
   );
 };
 
 export default App;
-
